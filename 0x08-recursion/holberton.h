ea17c56c9ca3e764df2ec4b9999ab2a5c7e9f5f5
Kashuhappy <knashipa@gmail.com>
2021-07-07 06:40:56 -0700

    All tasks done

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,22 +1,15 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
 int _putchar(char c);
-/* 0-puts_recursion.c */
-Void _puts_recursion(char *s);
-/* 1-print_rev_recursion.c */
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-/* 2-strlen_recursion.c */
 int _strlen_recursion(char *s);
-/* 3-factorial.c */
 int factorial(int n);
-/* 4-pow_recursion.c */
 int _pow_recursion(int x, int y);
-/* 5-sqrt_recursion.c */
+int square(int n, int val);
 int _sqrt_recursion(int n);
-/* 6-is_prime_number.c */
+int check_prime(int n, int resp);
 int is_prime_number(int n);
-/* 7-is_palindrome.c */
+int last_index(char *s);
+int check(char *s, int start, int end, int pair);
 int is_palindrome(char *s);
-/* 100-wildcmp.c */
+int checker(char *s1, char *s2, int a, int b, int wildUsed);
 int wildcmp(char *s1, char *s2);
-#endif
