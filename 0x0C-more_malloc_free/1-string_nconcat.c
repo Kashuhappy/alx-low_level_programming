146ae10a9abfb8351cf01c23688bfff6dbfce19f
Kashuhappy <knashipa@gmail.com>
2021-07-15 12:10:16 -0700

    All tasks done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,41 +1,46 @@
-#include "holberton.h"
+#include <stdlib.h>
 /**
-* _strlen - find length of a string
-* @s: string
-* Return: int
-*/
-unsigned int _strlen(char *s)
-{
-unsigned int size = 0;
-for (; s[size] != '\0'; size++)
-;
-return (size);
-}
-/**
-* *string_nconcat - concatenates two strings
-* @s1: string 1
-* @s2: string 2
-* @n: first bytes of s2 to be used
-* Return: pointer or NULL
+* string_nconcat - concatenates two strings.
+* @s1: pointer to first string.
+* @s2: pointer to second string from which we'll take first n bytes.
+* @n: n bytes of s2.
+*
+* Return: pointer to newly allocated space in memory.
+* NULL if malloc fails.
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j;
-char *m;
+char *nstr;
+unsigned int len1, len2, size, i, j;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-if (n < _strlen(s2))
-m = malloc(_strlen(s1) + n * sizeof(char) + 1);
-else
-m = malloc(_strlen(s1) + _strlen(s2) + 1);
-if (m == 0)
+len1 = 0;
+while (s1[len1] != '\0')
+len1++;
+len2 = 0;
+while (s2[len2] != '\0')
+len2++;
+if (n >= len2)
+n = len2;
+size = len1 + n;
+nstr = malloc((sizeof(char) * size)+1);
+if (nstr == NULL)
 return (NULL);
-for (i = 0; s1[i] != '\0'; i++)
-m[i] = s1[i];
-for (j = 0; s2[j] != '\0' && j < n; i++, j++)
-m[i] = s2[j];
-m[i] = '\0';
-return (m);
+i = 0;
+while (i < len1)
+{
+nstr[i] = s1[i];
+i++;
+}
+j = 0;
+while (i < size)
+{
+nstr[i] = s2[j];
+i++;
+j++;
+}
+nstr[i] = '\0';
+return (nstr);
 }
