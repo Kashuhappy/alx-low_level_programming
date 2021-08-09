858495facaf3e866b77f961a2d332a31ee247875
Kashuhappy <knashipa@gmail.com>
2021-08-09 12:10:38 -0700

    All tasks done

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -6,7 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 unsigned int lit_to_big_endian(unsigned int x);
